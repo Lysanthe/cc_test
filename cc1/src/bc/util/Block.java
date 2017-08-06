684930b1ee38f0bf9ced6edce37f8cb714e6bb88
martin <martin@ms-macbook-pro.fritz.box>
2017-08-06 15:30:20 +0200

    Implemented JSON for Block output

--- cc1/src/bc/util/Block.java
+++ cc1/src/bc/util/Block.java
@@ -3,13 +3,17 @@ package bc.util;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+
 public class Block {
 	public byte[] version = { 0x00, 0x00, 0x00, 0x01 };
 	public byte[] prevHash = new byte[32];
 	public byte[] merkleRoot = new byte[32];
 	public byte[] reserved = new byte[32];
 	public byte[] timestamp = new byte[8];
-	public byte[] bits = { (byte) 0x00, (byte) 0x00, (byte) 0xFF, (byte) 0xFF };
+	public byte[] bits = { (byte) 0x04, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF };
 	public byte[] nounce = new byte[32];
 	public byte[] hash = new byte[32];
 
@@ -46,25 +50,42 @@ public class Block {
 	public byte[] GetHash() {
 		return hash;
 	}
-	
+
 	// ------------------------------------------------------------------------
-	public boolean isHashValid()
-	{
+	public boolean isHashValid() {
 		byte[] calcHash = Util.hashBlock(this);
-		
+
 		return hash.equals(calcHash);
 	}
 
 	// ------------------------------------------------------------------------
 	public byte[] getTarget() {
-		
-		short exp = (short)(bits[0] & 0xFF);
-		byte[] mult = Arrays.copyOfRange(bits,1,4);
+
+		short exp = (short) (bits[0] & 0xFF);
+		byte[] mult = Arrays.copyOfRange(bits, 1, 4);
 		byte[] target = new byte[32];
-		
+
 		System.arraycopy(mult, 0, target, exp, 3);
+
+		return target;
+	}
+
+	// ------------------------------------------------------------------------
+	public JsonObject json() {
+		JsonObjectBuilder obj = Json.createObjectBuilder();
+
+		obj.add("version", Util.bytesToString(version));
+		obj.add("prevHash", Util.bytesToString(prevHash));
+		obj.add("merkleRoot"	, Util.bytesToString(merkleRoot));
+		obj.add("reserved", Util.bytesToString(reserved));
+		obj.add("timestamp", Util.bytesToString(timestamp));
+		obj.add("bits", Util.bytesToString(bits));
+		obj.add("nounce", Util.bytesToString(nounce));
+		obj.add("hash", Util.bytesToString(hash));
+				
+
 		
-		return target;		
+		return obj.build();
 	}
 
 }
--- cc1/src/bc/util/Start.java
+++ cc1/src/bc/util/Start.java
@@ -1,23 +1,37 @@
 package bc.util;
 
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.json.Json;
+import javax.json.JsonWriter;
+import javax.json.JsonWriterFactory;
+import javax.json.stream.JsonGenerator;
+
 import bc.util.mine.SHA256Miner;
 
 public class Start {
 
-	public static void main(String[] args) {	
-				Block block = new Block();		
-		
+	public static void main(String[] args) {
+		Block block = new Block();
+
 		SHA256Miner miner = new SHA256Miner(block);
-		System.out.println("Target: " + block.getTarget().toString());
 		System.out.println("Target: " + Util.bytesToString(block.getTarget()));
-		miner.start();	
-				
+		miner.start();
+
 		Util.hashBlock(block);
 		System.out.println(Util.bytesToString(block.GetHash()));
-		System.out.format("Number of retries: %,d",miner.getNumberOfPasses());
+		System.out.format("Number of retries: %,d", miner.getNumberOfPasses());
 		System.out.println();
-		System.out.println(Util.bytesToString(block.GetHeader()));
-		//String.format("%064x", new java.math.BigInteger(1, digest));
+	
+		StringWriter stringWriter = new StringWriter();
+		final Map<String, String> generatorConfig = new HashMap<>();
+		generatorConfig.put(JsonGenerator.PRETTY_PRINTING, "true");
+		JsonWriterFactory writerFactory = Json.createWriterFactory(generatorConfig);
+		JsonWriter writer = writerFactory.createWriter(stringWriter);
+		writer.writeObject(block.json());
+		writer.close();
+		System.out.println(stringWriter.getBuffer().toString());
 	}
-
 }
